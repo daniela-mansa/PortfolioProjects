38bb979917204aae4bb0ef708c590b0d9bc9a0bd
daniela-mansa <97484713+daniela-mansa@users.noreply.github.com>
2023-01-07 21:40:39 -0300

    Add files via upload

--- Portfolio Project - COVID.sql
+++ Portfolio Project - COVID.sql
@@ -1,3 +1,8 @@
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
 Select *
 from PortfolioProject..CovidDeaths
 order by 3,4 
